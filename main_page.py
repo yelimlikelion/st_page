9e5baed8abd048a13b2c010ce384e9308daa9551
yelimlikelion <105343281+yelimlikelion@users.noreply.github.com>
2022-06-08 16:31:05 +0900

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -24,7 +24,7 @@ if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data)
 
-st.subheader('Number of pickups by hour')
+st.subheader('유영이메롱')
 hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
 
